f803d974f92eec4d36f982d1205c002ceb6a8681
Ayca Akyol <aycaakyol3@gmail.com>
2022-06-08 23:17:27 +0300

    p2

---
+++ src/test/java/com/example/demo/SpringContextTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ContextConfiguration(locations={"classpath:WEB-INF/application-context.xml"})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringContextTest {
+    
+    @Test
+    public void *
+}
